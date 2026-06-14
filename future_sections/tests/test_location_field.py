cf6255f97cdf4d8d2584e30d242f07de1eae30e3
Avinash Kadaji <kadaji@gmail.com>
2026-06-14 01:13:33 +0000

    feat(location): drive teaching-form location dropdown from Available Locations setting

--- future_sections/tests/test_location_field.py
+++ future_sections/tests/test_location_field.py
@@ -1,6 +1,10 @@
-from django import forms
-from django.test import SimpleTestCase
+import json
 
+from django import forms
+from django.test import SimpleTestCase, TestCase
+
+from cis.models.settings import Setting
+from future_sections.future_sections.forms import TeacherCourseSectionForm
 from future_sections.future_sections.schemas import TeachingSectionFieldSchema
 from future_sections.future_sections.settings.future_sections import (
     future_sections as fs_setting_form,
@@ -39,3 +43,40 @@ class LocationSettingFieldTests(SimpleTestCase):
     def test_location_options_field_is_optional(self):
         self.assertFalse(
             fs_setting_form.base_fields['location_options'].required)
+
+
+class LocationFormWiringTests(TestCase):
+    def _make_setting(self, location_options='Main Campus|North High|Online',
+                      fields=('term', 'location')):
+        Setting.objects.create(
+            key='cis_future_sections',
+            value={
+                'teaching_form_config': json.dumps({
+                    'fields': list(fields),
+                    'required': ['term'],
+                }),
+                'location_options': location_options,
+            },
+        )
+
+    def test_location_is_choicefield_with_configured_options_when_visible(self):
+        self._make_setting()
+        form = TeacherCourseSectionForm()
+        field = form.fields['location']
+        from django import forms as djforms
+        self.assertIsInstance(field, djforms.ChoiceField)
+        self.assertEqual(field.choices[0], ('', '---------'))
+        self.assertIn(('Main Campus', 'Main Campus'), field.choices)
+        self.assertIn(('Online', 'Online'), field.choices)
+
+    def test_location_is_hidden_when_not_enabled(self):
+        self._make_setting(fields=('term',))
+        form = TeacherCourseSectionForm()
+        from django import forms as djforms
+        self.assertIsInstance(form.fields['location'].widget, djforms.HiddenInput)
+
+    def test_stored_location_not_in_options_is_preserved(self):
+        self._make_setting(location_options='Main Campus|Online')
+        form = TeacherCourseSectionForm(initial={'location': 'Legacy Room'})
+        values = {c[0] for c in form.fields['location'].choices}
+        self.assertIn('Legacy Room', values)
